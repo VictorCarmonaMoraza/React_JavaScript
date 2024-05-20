2edde877c4872c9665ddeed90f4fdf4dcd8f0d7e
Victo <victorcarmona9035@hotmail.com>
2024-05-20 13:11:09 +0100

    Variables y constantes

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,21 @@
+
+//Variables y Constantes
+const nombre = 'Fernando';
+const apellido = 'Herrera';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if(true){
+    const nombre = 'Peter';
+    let valorDado = 6;
+
+    console.log(valorDado);
+    console.log(nombre);
+}
+console.log(nombre);
+console.log(valorDado);
+
+//var: No se debe de usar...
